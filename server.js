691275ee2b94c4da5e8f01c93d3ecc04e153e148
Ali <ar.k@outlook.com>
2017-09-19 18:22:24 -0700

    complete the mini sprint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const Bear = require('./models.js');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
@@ -10,16 +11,44 @@ const server = express();
 server.use(bodyParser.json());
 
 
+// Post Request
+server.post('/bears', (req, res) => {
+  const { species, latinName } = req.body;
+  if (!species || !latinName) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must have species and latinName.' });
+    return;
+  }
+  const bear = new Bear({ species, latinName });
+  bear.save((err) => {
+    if (err) throw err;
+    res.json(bear);
+  });
+});
 
+// Get Request
+server.get('/bears', (req, res) => {
+  Bear.find({}, (err, bears) => {
+    if (err) {
+      res.status(STATUS_USER_ERROR);
+      res.json(err);
+    }
+    res.json(bears);
+  });
+});
 
+// Get Request by ID
 
-
-// TODO: write your server code here
-
-
-
-
-
+server.get('/bears/:id', (req, res) => {
+  const { id } = req.params;
+  Bear.findById(id, (err, data) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({ error: 'Can not find this bear' });
+    }
+    res.json(data);
+  });
+});
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
